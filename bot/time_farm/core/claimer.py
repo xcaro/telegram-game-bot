21fc874945f03c33566cdfe1a4105c34dd44ed8d
Nhan Vo <tinhnhanvh@gmail.com>
2024-06-22 16:32:49 +0700

    update

--- bot/time_farm/core/claimer.py
+++ bot/time_farm/core/claimer.py
@@ -45,60 +45,55 @@ class TimeFarmClaimer(BaseGame):
     async def run(self) -> None:
         tg_web_data = await self.get_tg_web_data()
         print(tg_web_data)
-        access_token_created_time = 0
         available = False
 
         async with aiohttp.ClientSession(headers=session_headers) as http_client:
             while True:
                 try:
-                    if time() - access_token_created_time >= 3600:
-                        login_data = await self.login(http_client=http_client, tg_web_data=tg_web_data)
+                    login_data = await self.login(http_client=http_client, tg_web_data=tg_web_data)
 
-                        http_client.headers["Authorization"] = f"Bearer {login_data['token']}"
-                        session_headers["Authorization"] = f"Bearer {login_data['token']}"
+                    http_client.headers["Authorization"] = f"Bearer {login_data['token']}"
+                    session_headers["Authorization"] = f"Bearer {login_data['token']}"
 
-                        level_num = int(login_data['level'])
-                        levelDescriptions = login_data['levelDescriptions']
+                    level_num = int(login_data['level'])
+                    levelDescriptions = login_data['levelDescriptions']
 
-                        access_token_created_time = time()
+                    tasks_data = await self.get_tasks_list(http_client=http_client)
 
-                        tasks_data = await self.get_tasks_list(http_client=http_client)
+                    for task in tasks_data:
+                        task_id = task["id"]
+                        task_title = task["title"]
+                        task_type = task["type"]
+                        if "submission" in task.keys():
+                            status = task["submission"]["status"]
+                            if status == "CLAIMED":
+                                continue
 
-                        for task in tasks_data:
-                            task_id = task["id"]
-                            task_title = task["title"]
-                            task_type = task["type"]
-                            if "submission" in task.keys():
-                                status = task["submission"]["status"]
-                                if status == "CLAIMED":
+                            if status == "COMPLETED":
+                                task_data_claim = await self.task_claim(http_client=http_client, task_id=task_id)
+                                if task_data_claim == "OK":
+                                    logger.success(f"{self.session_name} | Successful claim | "
+                                                   f"Task Title: <g>{task_title}</g>")
                                     continue
 
-                                if status == "COMPLETED":
-                                    task_data_claim = await self.task_claim(http_client=http_client, task_id=task_id)
-                                    if task_data_claim == "OK":
-                                        logger.success(f"{self.session_name} | Successful claim | "
-                                                       f"Task Title: <g>{task_title}</g>")
-                                        continue
+                        if task_type == "TELEGRAM":
+                            continue
 
-                            if task_type == "TELEGRAM":
-                                continue
+                        task_data_submiss = await self.task_submiss(http_client=http_client, task_id=task_id)
+                        if task_data_submiss != "OK":
+                            continue
 
-                            task_data_submiss = await self.task_submiss(http_client=http_client, task_id=task_id)
-                            if task_data_submiss != "OK":
-                                # logger.error(f"{self.session_name} | Failed Send Submission Task: {task_title}")
-                                continue
+                        task_data_x = await self.get_task_data(http_client=http_client, task_id=task_id)
+                        status = task_data_x["submission"]["status"]
+                        if status != "COMPLETED":
+                            logger.error(f"{self.session_name} | Task is not completed: {task_title}")
+                            continue
 
-                            task_data_x = await self.get_task_data(http_client=http_client, task_id=task_id)
-                            status = task_data_x["submission"]["status"]
-                            if status != "COMPLETED":
-                                logger.error(f"{self.session_name} | Task is not completed: {task_title}")
-                                continue
-
-                            task_data_claim_x = await self.task_claim(http_client=http_client, task_id=task_id)
-                            if task_data_claim_x == "OK":
-                                logger.success(f"{self.session_name} | Successful claim | "
-                                               f"Task Title: <g>{task_title}</g>")
-                                continue
+                        task_data_claim_x = await self.task_claim(http_client=http_client, task_id=task_id)
+                        if task_data_claim_x == "OK":
+                            logger.success(f"{self.session_name} | Successful claim | "
+                                           f"Task Title: <g>{task_title}</g>")
+                            continue
 
                     mining_data = await self.get_mining_data(http_client=http_client)
 
@@ -180,6 +175,8 @@ class TimeFarmClaimer(BaseGame):
                     logger.info(f"Sleep 1min")
                     await asyncio.sleep(delay=60)
 
+                await asyncio.sleep(delay=3600)
+
     async def get_mining_data(self, http_client: aiohttp.ClientSession):
         try:
             response = await http_client.get('https://tg-bot-tap.laborx.io/api/v1/farming/info')
@@ -238,7 +235,6 @@ class TimeFarmClaimer(BaseGame):
             return response.text
 
         except Exception as error:
-            # logger.error(f"{self.session_name} | Unknown error while claim task: {error}")
             await asyncio.sleep(delay=3)
 
     async def task_submiss(self, http_client: aiohttp.ClientSession, task_id: str):
@@ -250,7 +246,6 @@ class TimeFarmClaimer(BaseGame):
             return response.text
 
         except Exception as error:
-            # logger.error(f"{self.session_name} | Unknown error while submissions task: {error}")
             await asyncio.sleep(delay=3)
 
     async def start_mine(self, http_client: aiohttp.ClientSession):
@@ -265,7 +260,6 @@ class TimeFarmClaimer(BaseGame):
                 }
 
         except Exception as error:
-            # logger.error(f"{self.session_name} | Unknown error when start miner: {error}")
             await asyncio.sleep(delay=3)
 
             return {
@@ -288,7 +282,6 @@ class TimeFarmClaimer(BaseGame):
                 }
 
         except Exception as error:
-            # logger.error(f"{self.session_name} | Unknown error when Claiming: {error}")
             await asyncio.sleep(delay=3)
 
             return {
